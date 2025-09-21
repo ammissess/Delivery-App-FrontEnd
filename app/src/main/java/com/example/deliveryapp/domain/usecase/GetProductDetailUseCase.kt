b82e856140a5fe39853d20aaa67d707278c7a4da
Nerro <ammissess@gmail.com>
2025-09-21 22:45:26 +0700

    fix bug xung dot java.lang.NullPointerException vs jdk hilt

--- app/src/main/java/com/example/deliveryapp/domain/usecase/GetProductDetailUseCase.kt
+++ app/src/main/java/com/example/deliveryapp/domain/usecase/GetProductDetailUseCase.kt
@@ -3,7 +3,8 @@ package com.example.deliveryapp.domain.usecase
 import com.example.deliveryapp.data.remote.dto.ProductDto
 import com.example.deliveryapp.data.repository.ProductRepository
 import com.example.deliveryapp.utils.Resource
+import javax.inject.Inject
 
-class GetProductDetailUseCase(private val repo: ProductRepository) {
+class GetProductDetailUseCase @Inject constructor(private val repo: ProductRepository) {
     suspend operator fun invoke(id: Long): Resource<ProductDto> = repo.getProductById(id)
 }
--- app/src/main/java/com/example/deliveryapp/domain/usecase/PlaceOrderUseCase.kt
+++ app/src/main/java/com/example/deliveryapp/domain/usecase/PlaceOrderUseCase.kt
@@ -4,8 +4,10 @@ import com.example.deliveryapp.data.remote.dto.PlaceOrderRequestDto
 import com.example.deliveryapp.data.repository.OrderRepository
 import com.example.deliveryapp.utils.Resource
 
-class PlaceOrderUseCase(private val repo: OrderRepository) {
+import javax.inject.Inject
+
+class PlaceOrderUseCase @Inject constructor(private val repo: OrderRepository) {
     suspend operator fun invoke(req: PlaceOrderRequestDto): Resource<String> {
-        return repo.placeOrder(req) // đã là Resource<String>
+        return repo.placeOrder(req)
     }
 }
